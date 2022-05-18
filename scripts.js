f3b3a152a475a4bcbb815052cb8ac2ed4373ddc6
romus204 <romus204@ya.ru>
2022-05-18 15:47:54 +0700

    rebuild

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
-let bg = document.querySelector('.mouse-parallax-bg');
-window.addEventListener('mousemove', function(e) {
-    let x = e.clientX / window.innerWidth;
-    let y = e.clientY / window.innerHeight;
-    bg.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
-});
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+    if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        btn.innerHTML = "play";
+    }
+}
